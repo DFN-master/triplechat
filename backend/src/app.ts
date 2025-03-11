594ffc0a58cfb97a2a4d59e15268ab8f605f39d5
Douglas Rodrigues <douglas.rodrigues96.dr@gmai.com>
2025-03-11 10:48:18 -0300

    chore : Ajustado cors para aceitar qualquer origem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,7 +29,8 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    // origin: process.env.FRONTEND_URL
+    origin: "*"
   })
 );
 app.use(cookieParser());
