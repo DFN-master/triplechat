cf102c9e1894c3bec68b7e1528c3ed40c0cb4647
Douglas Rodrigues <douglas.rodrigues96.dr@gmai.com>
2025-06-24 14:01:37 -0300

    feat: Alterado versão minor

--- frontend/src/layout/MainListItems.js
+++ frontend/src/layout/MainListItems.js
@@ -545,7 +545,7 @@ const MainListItems = (props) => {
               </Hidden> 
               */}
               <Typography style={{ fontSize: "12px", padding: "10px", textAlign: "right", fontWeight: "bold" }}>
-                8.0.1
+                8.0.2
               </Typography>
             </React.Fragment>
             }
